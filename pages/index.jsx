110c79ade3f5ffa7f3a62fc89d485b8af91e83b7
Vasilis Papakonstantinou <vasilis.papakonstantinou@datawise.ai>
2023-11-22 12:53:54 +0200

    Add Swiper

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,11 @@ import Navigation from "./../components/Navigation/index.jsx";
 import SectionFirst from "./../components/SectionFirst/index.jsx";
 import SectionSecond from "./../components/SectionSecond/index.jsx";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Wrapper = ({ children }) => (
   <Stack
@@ -22,8 +27,25 @@ function LandingPage() {
   return (
     <Wrapper>
       <Navigation />
+      <Swiper
+        autoHeight={true}
+        direction={"vertical"}
+        slidesPerView={1}
+        mousewheel={true}
+        modules={[Mousewheel]}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <SwiperSlide
+          style={{ width: "100vw", height: "100%", position: "relative" }}
+        >
           <SectionFirst />
+        </SwiperSlide>
+        <SwiperSlide
+          style={{ width: "100vw", height: "100%", position: "relative" }}
+        >
           <SectionSecond />
+        </SwiperSlide>
+      </Swiper>
     </Wrapper>
   );
 }
